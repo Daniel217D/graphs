34ef928265da241c1cda88c6cdd480b4db13c62c
Daniil <daniel217032001@gmail.com>
2020-04-20 23:01:09 +0300

    Update helpers.js

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,3 +1,12 @@
-const distance = (sx, sy, ex, ey) => Math.sqrt((sx-ex)**2 + (sy-ey)**2);
+const distance = (sx, sy, ex, ey) => Math.sqrt((sx - ex) ** 2 + (sy - ey) ** 2);
 
-export {distance};
+const angle = (sx, sy, ex, ey) => Math.atan2(ey - sy, ex - sx);
+
+const direction = (...args) => {
+    const ang = angle(...args);
+    return {x: Math.cos(ang), y: Math.sin(ang)};
+};
+
+const angleToDirection = (ang) => ({x: Math.cos(ang), y: Math.sin(ang)});
+
+export {distance, direction, angle, angleToDirection};
