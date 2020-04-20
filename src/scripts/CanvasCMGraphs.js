34d3edd577fa68a4d713c15cb5ccaefef10ea0c9
Daniil <daniel217032001@gmail.com>
2020-04-20 23:01:36 +0300

    Add method gArrow to CanvasCMGraphs

--- src/scripts/CanvasCMGraphs.js
+++ src/scripts/CanvasCMGraphs.js
@@ -1,12 +1,27 @@
 import {CanvasCM} from 'canvas-chaining-method';
+import {direction, angle, angleToDirection} from './helpers';
 
 class CanvasCMGraphs extends CanvasCM {
+    gArrow = (sx, sy, ex, ey, r) => {
+        const dir = direction(sx, sy, ex, ey);
+        const sxArrow = ex - dir.x * r;
+        const syArrow = ey - dir.y * r;
+        const exArrow = ex - dir.x * 2 * r;
+        const eyArrow = ey - dir.y * 2 * r;
+
+        const arrow1 = angleToDirection(angle(sxArrow, syArrow, exArrow, eyArrow) - ( 30 * Math.PI / 180));
+        const arrow2 = angleToDirection(angle(sxArrow, syArrow, exArrow, eyArrow) + ( 30 * Math.PI / 180));
+
+        return this.gLine(sx, sy, ex, ey)
+            .gLine(sxArrow, syArrow, sxArrow + arrow1.x * r /2 , syArrow + arrow1.y * r / 2)
+            .gLine(sxArrow, syArrow, sxArrow + arrow2.x * r / 2, syArrow + arrow2.y * r / 2);
+    };
+
     gLine = (sx, sy, ex, ey) => {
         return this.beginPath()
             .line(sx, sy, ex, ey)
             .closePath()
             .stroke();
-
     };
 
     gDot = (x, y, r, color, text) => {
