9f1c5477377df1ce5b320ee3f69a50ff7076c1dd
Daniil <daniel217032001@gmail.com>
2020-04-26 01:21:37 +0300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,57 @@ const $gcanvas = $cg(document.getElementById('canvas')).setSize();
 const cursor = new Cursor($gcanvas.get('canvas'));
 const dots = new Dots($gcanvas);
 
+// dots.add(300, 300); //1
+// dots.add(600, 300);
+// dots.add(600, 600);
+// dots.add(350, 700);
+// dots.add(800, 700); //5
+// dots.add(800, 200);
+// dots.add(800, 300);
+// dots.add(900, 400);
+// dots.add(800, 500);
+// dots.add(200, 500); //10
+// dots.addPath(1,2);
+// dots.addPath(1,10);
+// dots.addPath(1,4);
+// dots.addPath(2,3);
+// dots.addPath(3,2);
+// dots.addPath(3,5);
+// dots.addPath(3,4);
+// dots.addPath(2,4);
+// dots.addPath(5,2);
+// dots.addPath(5,4);
+// dots.addPath(6,2);
+// dots.addPath(6,7);
+// dots.addPath(7,8);
+// dots.addPath(8,5);
+// dots.addPath(8,9);
+// dots.addPath(9,5);
+// dots.addPath(10,4);
+
+
+dots.add(300, 300);
+dots.add(600, 300);
+dots.add(600, 600);
+dots.add(300, 600);
+dots.addPath(1,2);
+dots.addPath(1,3);
+dots.addPath(1,4);
+dots.addPath(2,1);
+dots.addPath(3,1);
+dots.addPath(4,3);
+
+// dots.add(300,450);
+// dots.add(450,300);
+// dots.add(600,450);
+// dots.add(525,600);
+// dots.add(375,600);
+// dots.addPath(1,2);
+// dots.addPath(1,3);
+// dots.addPath(1,5);
+// dots.addPath(2,3);
+// dots.addPath(4,5);
+
 
 $gcanvas.on('mousedown', () => {
     const dot = dots.getByCoordinates(cursor.x, cursor.y, {r: 0});
@@ -68,6 +119,11 @@ $gcanvas.on('mousedown', () => {
 
 const clearBtn = document.getElementById("canvas-clear");
 clearBtn.addEventListener("click", () => dots.clear());
+
+window.test1 = () => {
+    console.log(dots.cores());
+    console.log(dots.cores(true));
+};
 
 function Render() {
     requestAnimationFrame(Render);
