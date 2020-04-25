1023909f3f81f36d02b176de50e85bcb982c9c7b
Daniil <daniel217032001@gmail.com>
2020-04-26 02:13:42 +0300

    add output block

---
+++ src/scripts/Status.js
@@ -0,0 +1,33 @@
+export default class Status {
+    constructor(htmlElement) {
+        this.field = htmlElement;
+    }
+
+    status;
+
+    set = (status) => this.status = status;
+
+    print(status = false) {
+        if (status !== false) this.set(status);
+        let text = "";
+
+        if(status.length === 0) {
+            text = "Пустое множество"
+        }else if(Array.isArray(status)) {
+            text = "<ul>";
+            status.forEach(sub => {
+                text += "<li>";
+                sub.forEach(el => {
+                   text += el + ", ";
+                });
+                text = text.substring(0, text.length-2);
+                text += ";</li>";
+            });
+            text += "</ul>";
+        } else {
+            text = status;
+        }
+
+        this.field.innerHTML = text;
+    }
+}
